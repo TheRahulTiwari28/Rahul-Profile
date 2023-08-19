c54148259d7e96f6c84a239c6069bf34ea6879e8
tiwarirahul28 <tiwarirahul09072019@gmail.com>
2023-08-19 15:49:23 +0530

    first commit

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -6,19 +6,25 @@ export default function AboutMe() {
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
-          <p className="section--title">About</p>
+          <p className="section--title">Let Me Introduce Myself</p>
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
+          I am a Front-End developer from India. I create professional websites. I love art and always try to 
+          show unique views to the audience through my design.
+          {/* Hi there! I'm Rahul Tiwari, a self-taught front-end web developer from India with a strong 
+          background in HTML, CSS, and JavaScript. I'm also proficient in a variety of front-end frameworks, 
+          such as React. I'm passionate about building beautiful, user-friendly interfaces that provide real 
+          value and interest to the end user. I've completed several creative and user-friendly front-end 
+          projects, and I'm always looking for new challenges.I'm currently working as a front-end developer 
+          at Tunicalabs Media, where I'm responsible for building and maintaining the company's website.  */}
           </p>
-          <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae?
-          </p>
+          {/* <p className="hero--section-description">
+          I'm also available for freelance work, and I'm always looking for new opportunities to learn and 
+          grow. If you have a project that you think I'd be a good fit for, please don't hesitate to contact 
+          me.
+          </p> */}
+          <button className="btn btn-primary">Work</button>
+          <button className="btn btn-primary">Read More</button>
         </div>
       </div>
     </section>
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,18 +3,18 @@ export default function HeroSection() {
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hey, I'm John</p>
+          <p className="section--title">Hey, Rahul Tiwari</p>
           <h1 className="hero--section--title">
-            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <span className="hero--section-title--color">Front-End</span>{" "}
             <br />
             Developer
           </h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
+            I am Front-End developer.
+            <br /> I love to design and make new web experiences for the people.
           </p>
         </div>
-        <button className="btn btn-primary">Get In Touch</button>
+        {/* <button className="btn btn-primary">Get In Touch</button> */}
       </div>
       <div className="hero--section--img">
         <img src="./img/hero_img.png" alt="Hero Section" />
